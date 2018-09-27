024c9d4e4875e105b4992e34a8d274aa6344ea8b
yhmiao <568888047@qq.com>
2018-09-27 10:06:48 +0800

    delete helloworld proj

--- hybirdApp/ios/index.js
+++ hybirdApp/ios/index.js
@@ -3,17 +3,9 @@ import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
 class RNHighScores extends React.Component {
   render() {
-    var contents = this.props["scores"].map(score => (
-      // <Text key={score.name}>
-      //   {score.name}:{score.value}
-      //   {"\n"}
-      // </Text>
-      <Text> 这是一个富文本{"\n"} sad </Text>
-    ));
     return (
       <View style={styles.container}>
         <Text style={styles.highScoresTitle}>我是头部</Text>
-        <Text style={styles.scores}>{contents}</Text>
       </View>
     );
   }
